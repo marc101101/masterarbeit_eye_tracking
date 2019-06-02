9295075a2314043aeaca34d690f804bd0aad407b
Markus Guder <progamer101101@gmx.de>
2019-06-02 15:30:39 +0200

    Updated Client py

--- client/client.py
+++ client/client.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python
 import sys
-import requests
-import json
+import socketio
 
 
 class Client:
 
-    ip_address = ""
     client_name = ""
+    sio = None
 
     def __init__(self, ip_address, client_name):
-        self.ip_address = ip_address
         self.client_name = client_name
+        sio = socketio.Client()
+        sio.connect('http://' + ip_address + ':5000')
+
         self.watch_data_stream()
 
     def watch_data_stream(self):
@@ -36,8 +37,6 @@ class Client:
                                 raise e
 
                             counter = counter + 1
-                        #if (len(frame_to_push) >= 23):
-                            #print("Message received: " + str(frame_to_push))
                         try:
                             print("Message received: " + str(buff))
                             self.push_to_server(frame_to_push)
@@ -55,19 +54,8 @@ class Client:
             pass
         print("End of Log: " + str(k))
 
-    def push_to_server(self, frame_to_push):
-        url = "http://" + str(self.ip_address) + ":5000/gazeDetection"
-
-        headers = {
-            'Content-Type': "application/json"
-        }
-
-        response = requests.request("POST",
-                                    url,
-                                    data=json.dumps(self.format_frame_to_push(frame_to_push)),
-                                    headers=headers)
-
-        print(response.text)
+    def push_to_server(self, message):
+        self.sio.emit('message', self.format_frame_to_push(message))
 
     def format_frame_to_push(self, frame_to_push):
         return {
