0fddf64576422ab71d648989d036ba18e056f928
Markus Guder <progamer101101@gmx.de>
2019-06-09 18:22:21 +0200

    added 3D

--- start_scripts/1_CAM.sh
+++ start_scripts/1_CAM.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-../../OpenFace/build/bin/FeatureExtraction -device 0 -gaze -q | python3  client/client.py 192.168.0.191 cam_1
+../../OpenFace/build/bin/FeatureExtraction -device 0 -gaze -q | python3  ../client/client.py 192.168.0.191 cam_1
