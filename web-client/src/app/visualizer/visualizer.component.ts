488d4fc6ea852daba74435cb50499a93189a799d
Markus Guder <progamer101101@gmx.de>
2019-06-09 17:08:05 +0200

    finished visualizer

--- web-client/src/app/visualizer/visualizer.component.ts
+++ web-client/src/app/visualizer/visualizer.component.ts
@@ -23,6 +23,28 @@ export class VisualizerComponent implements OnInit {
   file: any;
   data: any;
   data_available = false;
+  keys;
+  selectedValue = "frame";
+  chart;
+  clients = [
+    {
+      name: "cam_1", 
+      color: "rgb(220,20,60)"
+    },
+    {
+      name: "cam_2", 
+      color: "rgb(141, 192, 0)"
+    },
+    {
+      name: "cam_3", 
+      color: "rgb(141, 120, 251)"
+    },
+    {
+      name: "cam_4", 
+      color: "rgb(232, 235, 0)"
+    },
+  ]
+
 
   fileChanged(e) {
     this.file = e.target.files[0];
@@ -33,10 +55,19 @@ export class VisualizerComponent implements OnInit {
     let fileReader = new FileReader();
     fileReader.onload = (e) => {
       this.data = this.csvJSON(fileReader.result);
+      this.keys = Object.keys(this.data[0]);      
       this.initVisualization();
     }
     fileReader.readAsText(this.file);
   }
+
+  changed(e){
+    // event comes as parameter, you'll have to find selectedData manually
+    // by using e.target.data
+    this.chart.destroy();
+    this.initMainGraph(this.parseData(this.selectedValue));
+  } 
+
 
   csvJSON(csv){
 
@@ -65,40 +96,85 @@ export class VisualizerComponent implements OnInit {
 
   initVisualization(){
     this.data_available = true;
-    this.initMainGraph(this.parseData("pose_Rx"));
+    this.initMainGraph(this.parseData(this.selectedValue));
   }
 
   parseData(value){
     let retVal = [];
-    this.data.forEach(element => {
-      retVal.push({
-        'x': new Date(parseInt(element.server_timestamp)),
-        'y': element[value]
-      });
+  
+    this.clients.forEach(element => {      
+        retVal.push( {
+          label: element.name,
+          data: this.createDataRow(element.name, value),
+          type: 'line',
+          borderColor: element.color,
+          pointRadius: 0,
+          fill: false,
+          lineTension: 0,
+          borderWidth: 2
+        });
     });
+
     return retVal; 
+  }
+
+  createDataRow(client, value){
+    let retVal = [];
+    this.data.forEach(element => { 
+      console.log(element);
+      
+      if(element.client_id == client){    
+        retVal.push({
+          't': parseInt(element['server_timestamp'])*1000,
+          'y': parseFloat(element[value])
+        });
+      }
+    });
+    return retVal;
   }
 
   initMainGraph(parsed_data){
 
-    console.log(parsed_data);
-
     let context: CanvasRenderingContext2D = this.graphCanvas.nativeElement.getContext("2d");
 
-    new chartJs(context, {
-      type: 'line',
-      data: parsed_data,
-      options: {
-        scales: {
-            xAxes: [{
-                type: 'time',
-                time: {
-                    unit: 'millisecond'
-                }
-            }]
-        }
-    }
-    });  
+    this.chart = new chartJs(context, {
+			type: 'bar',
+			data: {
+				datasets: parsed_data
+			},
+			options: {
+				scales: {
+					xAxes: [{
+						type: 'time',
+						distribution: 'series',
+						ticks: {
+							source: 'data',
+							autoSkip: true
+						}
+					}],
+					yAxes: [{
+						scaleLabel: {
+							display: true,
+							labelString: ''
+						}
+					}]
+				},
+				tooltips: {
+					intersect: false,
+					mode: 'index',
+					callbacks: {
+						label: function(tooltipItem, myData) {
+							var label = myData.datasets[tooltipItem.datasetIndex].label || '';
+							if (label) {
+								label += ': ';
+							}
+							label += parseFloat(tooltipItem.value).toFixed(2);
+							return label;
+						}
+					}
+				}
+			}
+		});  
   }
 
   ngOnInit() {
