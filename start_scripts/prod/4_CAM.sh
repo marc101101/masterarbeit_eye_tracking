52692b1175666d56c71238e030888762579a5cb0
Markus Guder <progamer101101@gmx.de>
2019-06-10 22:24:46 +0200

    Added -tracked to start scripts

--- start_scripts/prod/4_CAM.sh
+++ start_scripts/prod/4_CAM.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-../../OpenFace/build/bin/FeatureExtraction -q -device 0 -gaze | python3  ../../client/client.py 192.168.188.20 cam_4
+../../OpenFace/build/bin/FeatureExtraction -tracked -q -device 0 -gaze | python3  ../../client/client.py 192.168.188.20 cam_4
