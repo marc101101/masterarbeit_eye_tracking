19aea361cd47bbf1d4b53fc7e3bfcaca899646ec
Markus Guder <progamer101101@gmx.de>
2019-06-10 22:22:15 +0200

    added backup scripts

---
+++ download_scripts/0_download_all.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sh ./1_download.sh && sh ./2_download.sh && sh ./3_download.sh && sh ./4_download.sh
---
+++ download_scripts/3_download.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sshpass -p "Ironmann27" scp -r pi@192.168.188.22:~/masterarbeit_eye_tracking/start_scripts/prod/processed /Users/markusguder/Desktop/backup/client_3
