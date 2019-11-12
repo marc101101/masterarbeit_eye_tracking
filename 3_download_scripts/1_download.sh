d80ce2349e91b95ee6e61ec35ce79f0d386053de
Markus Guder <progamer101101@gmx.de>
2019-11-12 20:18:24 +0100

    add

--- 3_download_scripts/1_download.sh
+++ 3_download_scripts/1_download.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sshpass -p "Ironmann27" scp -r pi@192.168.188.21:~/workspace/masterarbeit_eye_tracking/start_scripts/prod/processed /Users/markusguder/Desktop/backup/client_1
+sshpass -p "PASSWORD" scp -r pi@192.168.188.21:~/workspace/masterarbeit_eye_tracking/start_scripts/prod/processed /Users/markusguder/Desktop/backup/client_1
